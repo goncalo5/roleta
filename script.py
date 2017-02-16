029f0917458300330f7cdec14cb9056669f51ca0
goncalo andrade <goncalo@Joaos-MacBook-Pro.local>
2017-02-16 07:34:01 +0000

    first commit: add script and constats files

---
+++ script.py
@@ -0,0 +1,12 @@
+from Tkinter import *
+from constants import *
+
+class Game(object):
+    def __init__(self):
+        self.i = Tk()
+        
+        self.i.mainloop()
+
+
+if __name__ == '__main__':
+    Game()
